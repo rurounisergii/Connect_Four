f2f2c59c3859c318f9ca9c72263c02b31c77f8e2
Ilya Ivanov <contact@ilyaivanov.org>
2015-03-02 16:41:47 +0000

    minimax() now assigns value to parents based on value of leafs

--- PairProgrammingConnectFour/src/connectFour/AI.java
+++ PairProgrammingConnectFour/src/connectFour/AI.java
@@ -1,108 +1,127 @@
 package connectFour;
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 /**
- * An instance represents a Solver that intelligently determines
- * Moves using the Minimax algorithm.
+ * An instance represents a Solver that intelligently determines Moves using the
+ * Minimax algorithm.
  */
 public class AI implements Solver {
 
-    private Player player; // the current player
+	private Player player; // the current player
 
-    /**
-     * The depth of the search in the game space when evaluating moves.
-     */
-    private int depth;
+	/**
+	 * The depth of the search in the game space when evaluating moves.
+	 */
+	private int depth;
 
-    /**
-     * Constructor: an instance with player p who searches to depth d
-     * when searching the game space for moves.
-     */
-    public AI(Player p, int d) {
-        player = p;
-        depth = d;
-    }
+	/**
+	 * Constructor: an instance with player p who searches to depth d when
+	 * searching the game space for moves.
+	 */
+	public AI(Player p, int d) {
+		player = p;
+		depth = d;
+	}
 
-    /**
-     * See Solver.getMoves for the specification.
-     */
-    @Override
-    public Move[] getMoves(Board b) {
-        // TODO
-        return null;
-    }
+	/**
+	 * See Solver.getMoves for the specification.
+	 */
+	@Override
+	public Move[] getMoves(Board b) {
+		// TODO
+		return null;
+	}
 
-    /**
-     * Generate the game tree with root s of depth d.
-     * The game tree's nodes are State objects that represent the state of a game
-     * and whose children are all possible States that can result from the next move.
-     * <p/>
-     * NOTE: this method runs in exponential time with respect to d.
-     * With d around 5 or 6, it is extremely slow and will start to take a very
-     * long time to run.
-     * <p/>
-     * Note: If s has a winner (four in a row), it should be a leaf.
-     */
-    public static void createGameTree(State s, int d) {
-        // Note: This method must be recursive, recurse on d,
-        // which should get smaller with each recursive call
-    	if (d != 0){
-    		if (s.getBoard().hasConnectFour() == null){
-    			s.initializeChildren();
-	    		for(int i = 0; i < s.getChildren().length; i++){
-	    			createGameTree(s.getChildren()[i], d -1);
-	    		}
-    		}
-    	}
-    }
+	/**
+	 * Generate the game tree with root s of depth d. The game tree's nodes are
+	 * State objects that represent the state of a game and whose children are
+	 * all possible States that can result from the next move.
+	 * <p/>
+	 * NOTE: this method runs in exponential time with respect to d. With d
+	 * around 5 or 6, it is extremely slow and will start to take a very long
+	 * time to run.
+	 * <p/>
+	 * Note: If s has a winner (four in a row), it should be a leaf.
+	 */
+	public static void createGameTree(State s, int d) {
+		// Note: This method must be recursive, recurse on d,
+		// which should get smaller with each recursive call
+		if (d != 0) {
+			if (s.getBoard().hasConnectFour() == null) {
+				s.initializeChildren();
+				for (int i = 0; i < s.getChildren().length; i++) {
+					createGameTree(s.getChildren()[i], d - 1);
+				}
+			}
+		}
+	}
 
-    /**
-     * Call minimax in ai with state s.
-     */
-    public static void minimax(AI ai, State s) {
-    	createGameTree(s, ai.depth);
-    	ai.minimax(s);
-    }
+	/**
+	 * Call minimax in ai with state s.
+	 */
+	public static void minimax(AI ai, State s) {
+		createGameTree(s, ai.depth);
+		ai.minimax(s);
+	}
 
-    /**
-     * State s is a node of a game tree (i.e. the current State of the game).
-     * Use the Minimax algorithm to assign a numerical value to each State of the
-     * tree rooted at s, indicating how desirable that java.State is to this player.
-     */
-    public void minimax(State s) {
-    	if (s.getChildren().length != 0) {
-    		s.setValue(evaluateBoard(s.getBoard()));
+	/**
+	 * State s is a node of a game tree (i.e. the current State of the game).
+	 * Use the Minimax algorithm to assign a numerical value to each State of
+	 * the tree rooted at s, indicating how desirable that java.State is to this
+	 * player.
+	 */
+	public void minimax(State s) {
+    	if (s.getChildren().length != 0) { // parents
     		Arrays.stream(s.getChildren()).forEach(x -> minimax(x));
+    		if (player == s.getPlayer()) { // max
+    			Optional<Integer> maxVal = Arrays.stream(s.getChildren()).map(x -> x.getValue()).max(Integer::compare);
+    			if (maxVal.isPresent()){
+    				s.setValue(maxVal.get());
+//    				System.out.println(s.getValue());
+    			}
+    		} else { // min
+    			Optional<Integer> minVal = Arrays.stream(s.getChildren()).map(x -> x.getValue()).min(Integer::compare);
+    			if (minVal.isPresent()){
+    				s.setValue(minVal.get());
+    				System.out.println(s.getValue());
+    			}
+    		}
+    		s.setValue(evaluateBoard(s.getBoard()));
+    	} else { // leafs
+    		s.setValue(evaluateBoard(s.getBoard()));
     	}
     }
 
-    /**
-     * Evaluate the desirability of Board b for this player
-     * Precondition: b is a leaf node of the game tree (because that is most
-     * effective when looking several moves into the future).
-     */
-    public int evaluateBoard(Board b) {
-        Player winner = b.hasConnectFour();
-        int value = 0;
-        if (winner == null) {
-            // Store in sum the value of board b. 
-            List<Player[]> locs = b.winLocations();
-            for (Player[] loc : locs) {
-                for (Player p : loc) {
-                    value += (p == player ? 1 : p != null ? -1 : 0);
-                }
-            }
-        } else {
-            // There is a winner
-            int numEmpty = 0;
-            for (int r = 0; r < Board.NUM_ROWS; r = r + 1) {
-                for (int c = 0; c < Board.NUM_COLS; c = c + 1) {
-                    if (b.getTile(r, c) == null) numEmpty += 1;
-                }
-            }
-            value = (winner == player ? 1 : -1) * 10000 * numEmpty;
-        }
-        return value;
-    }
+	/**
+	 * Evaluate the desirability of Board b for this player Precondition: b is a
+	 * leaf node of the game tree (because that is most effective when looking
+	 * several moves into the future).
+	 */
+	public int evaluateBoard(Board b) {
+		Player winner = b.hasConnectFour();
+		int value = 0;
+		if (winner == null) {
+			// Store in sum the value of board b.
+			List<Player[]> locs = b.winLocations();
+			for (Player[] loc : locs) {
+				for (Player p : loc) {
+					value += (p == player ? 1 : p != null ? -1 : 0);
+				}
+			}
+		} else {
+			// There is a winner
+			int numEmpty = 0;
+			for (int r = 0; r < Board.NUM_ROWS; r = r + 1) {
+				for (int c = 0; c < Board.NUM_COLS; c = c + 1) {
+					if (b.getTile(r, c) == null)
+						numEmpty += 1;
+				}
+			}
+			value = (winner == player ? 1 : -1) * 10000 * numEmpty;
+		}
+		return value;
+	}
 }
--- PairProgrammingConnectFour/src/unitTests/AiTest.java
+++ PairProgrammingConnectFour/src/unitTests/AiTest.java
@@ -12,11 +12,28 @@ import connectFour.State;
 
 public class AiTest {
 
-	@Test
+//	@Test
 	public void createGameTreeTester() {
 		Board testerBoard = new Board();
 		State testerState = new State(Player.YELLOW, testerBoard, null);
 		AI.createGameTree(testerState, 4);
+		testerState.writeToFile();
+		// assertEquals(Player.YELLOW,
+		// testerState.getChildren()[0].getBoard().getTile(5, 6));
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void minimaxTester() {
+		Board testerBoard = new Board();
+		Game.fillColumn(testerBoard, 0, Board.NUM_ROWS, Player.RED);
+		Game.fillColumn(testerBoard, 1, Board.NUM_ROWS, Player.YELLOW);
+		Game.fillColumn(testerBoard, 2, Board.NUM_ROWS, Player.RED);
+		Game.fillColumn(testerBoard, 3, Board.NUM_ROWS, Player.RED);
+		Game.fillColumn(testerBoard, 4, Board.NUM_ROWS, Player.RED);
+		Game.fillColumn(testerBoard, 5, Board.NUM_ROWS, Player.YELLOW);
+		State testerState = new State(Player.YELLOW, testerBoard, null);
+		AI.minimax(new AI(Player.YELLOW, 6), testerState);
 		testerState.writeToFile();
 		// assertEquals(Player.YELLOW,
 		// testerState.getChildren()[0].getBoard().getTile(5, 6));
