b66260a3d8c384c25ce337e7d04edb68f84c9162
Sergio <rurounisergii@hotmail.com>
2015-03-05 23:09:08 +0000

    made another test in BoardTest

--- PairProgrammingConnectFour/src/unitTests/BoardTest.java
+++ PairProgrammingConnectFour/src/unitTests/BoardTest.java
@@ -1,7 +1,11 @@
 package unitTests;
 
 import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
 import org.junit.*;
+
 import connectFour.*;
 
 /**
@@ -35,10 +39,58 @@ public class BoardTest {
 		Move errorMove = new Move(Player.RED, 2); 
 		testerBoard.makeMove(errorMove); // make a move to add to the filled up column, should be an exception
 	}
+
+	@Test public void getPossibleMoves(){
+		Board testerBoard = new Board();
+		//fully fill up column 0 and 6
+		testerBoard.fillColumn(0, Board.NUM_ROWS, Player.RED);
+		testerBoard.fillColumn(6,Board.NUM_ROWS, Player.RED);
+		//As 2 of the 7 columns have been filled, Player.RED should have 5 possible moves,
+		//i.e. inserting into columns 1,2,3,4 or 5
+		Move[] expected = {new Move(Player.RED, 1), new Move(Player.RED, 2), new Move(Player.RED, 3), new Move(Player.RED, 4), new Move(Player.RED, 5)};
+		Move[] result = testerBoard.getPossibleMoves(Player.RED);
+		assertEquals(expected.length, result.length);
+		boolean sameArrays = true;
+		for (int x = 0; x < expected.length; x++){
+			if (expected[x].getColumn() != result[x].getColumn() ||expected[x].getPlayer() != result[x].getPlayer()){
+				sameArrays = false;
+			}
+		}
+		assertEquals(true, sameArrays);
+	
+	}
 	
 	@Test
-	public void getPossibleMovesTest(){
+	public void getPossibleMovesTestWithNoPossibleMoves(){
+		Board testerBoard = new Board();
+		testerBoard.fillColumn(1, Board.NUM_ROWS, Player.RED);
+		testerBoard.fillColumn(2, Board.NUM_ROWS, Player.RED);
+		testerBoard.fillColumn(3, Board.NUM_ROWS, Player.RED);
+		//3 columns have been filled up. There are 3 red pieces at the bottom
+		//so by adding one more red piece to column 4, red wins
+		testerBoard.makeMove(new Move(Player.RED, 4));
+		//As there is a winner, Player.YELLOW has no possible moves
+		//and getPossibleMoves should return an empty array
+		assertEquals(0, (testerBoard.getPossibleMoves(Player.YELLOW)).length);
 		
 	}
+	@Test
+	public void getPossibleMovesTestWithBoardFull(){
+		Board testerBoard = new Board();
+		//fill up the whole board
+		testerBoard.fillColumn(0, Board.NUM_ROWS, Player.RED);
+		testerBoard.fillColumn(1, Board.NUM_ROWS, Player.RED);
+		testerBoard.fillColumn(2, Board.NUM_ROWS, Player.RED);
+
+		testerBoard.fillColumn(4, Board.NUM_ROWS, Player.RED);
+		testerBoard.fillColumn(5, Board.NUM_ROWS, Player.RED);
+		testerBoard.fillColumn(6, Board.NUM_ROWS, Player.RED);
+		//getPossibleMoves should return an empty array as the board is full
+		assertEquals(0, testerBoard.getPossibleMoves(Player.YELLOW));
+	}
+	
+	//check must be in increasing column order
+	//if all columns are full - return array of size 0
+	//return array of size of possible moves
 
 }
