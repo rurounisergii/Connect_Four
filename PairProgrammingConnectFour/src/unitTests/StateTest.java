82667e4f6f02c9e11c205a32664407cef97aa1a6
Sergio <rurounisergii@hotmail.com>
2015-03-05 23:39:24 +0000

    added another test for initializeChildren()

--- PairProgrammingConnectFour/src/unitTests/StateTest.java
+++ PairProgrammingConnectFour/src/unitTests/StateTest.java
@@ -40,4 +40,27 @@ public class StateTest {
 		assertEquals(1, testerState.getChildren().length);
 	}
 
+	/**
+	 * Same as the test above, but this time, the board is set up so there are 2 possible moves
+	 * for the next player and therefore 2 children states should be initialised
+	 */
+	@Test
+	public void initializeChildrenTest2() {
+		Board testerBoard = new Board();
+		testerBoard.fillColumn(0, Board.NUM_ROWS - 1, Player.RED); // a possible move to add to column 0
+		testerBoard.fillColumn(1, Board.NUM_ROWS, Player.YELLOW);
+		testerBoard.fillColumn(2, Board.NUM_ROWS, Player.RED);
+		testerBoard.fillColumn(3, Board.NUM_ROWS, Player.RED);
+		testerBoard.fillColumn(4, Board.NUM_ROWS, Player.RED);
+		testerBoard.fillColumn(5, Board.NUM_ROWS, Player.YELLOW);
+		State testerState = new State(Player.YELLOW, testerBoard, null);
+		testerState.initializeChildren();
+		//2 possible states:
+		assertEquals(2, testerState.getChildren().length);
+		assertEquals(Player.YELLOW, testerState.getChildren()[0].getBoard().getTile(0, 0));
+		assertEquals(Player.YELLOW, testerState.getChildren()[1].getBoard().getTile(5, 6));
+		assertEquals(null, testerState.getChildren()[0].getBoard().getTile(4, 6));
+		
+	}
+
 }
