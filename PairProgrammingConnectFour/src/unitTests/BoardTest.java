73b51d857703a648b544247c9d8e7498e5221d43
Sergio <rurounisergii@hotmail.com>
2015-03-05 21:15:48 +0000

    added a makeMove() test to BoardTest

--- PairProgrammingConnectFour/src/unitTests/BoardTest.java
+++ PairProgrammingConnectFour/src/unitTests/BoardTest.java
@@ -1,8 +1,8 @@
 package unitTests;
 
 import static org.junit.Assert.*;
-
-import org.junit.Test;
+import org.junit.*;
+import connectFour.*;
 
 /**
  * 
@@ -12,8 +12,21 @@ import org.junit.Test;
 public class BoardTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void makeMoveTest(){
+		//make an empty board
+		Board testerBoard = new Board();
+		//make a move which will be applied to the board using mskeMove()
+		Move firstMove = new Move(Player.RED, 3);
+		testerBoard.makeMove(firstMove);
+		//check that the move has been made
+		assertEquals(Player.RED, testerBoard.getTile(5, 3));
+		//do the same again - check multiple moves can be made
+		Move secondMove = new Move(Player.YELLOW, 5);
+		testerBoard.makeMove(secondMove);
+		assertEquals(Player.YELLOW, testerBoard.getTile(5, 5));
+		Move thirdMove = new Move(Player.RED, 3);
+		testerBoard.makeMove(thirdMove);
+		assertEquals(Player.RED, testerBoard.getTile(4,3));
 	}
 
 }
